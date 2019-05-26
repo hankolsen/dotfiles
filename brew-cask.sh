0d1137358159546535903a2cd90f8185642de20d
hankolsen <henrik@hankolsen.com>
2019-05-26 22:28:02 +0200

    Update brew-cask.sh

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 brew cask install alfred
+brew cask install atext
+brew cask install bankid
 brew cask install dropbox
 brew cask install google-chrome
 brew cask install hyper
+brew cask install little-snitch
+brew cask install micro-snitch
 brew cask install mullvadvpn
+brew cask install postman
+brew cask install sizeup
 brew cask install sublime-text
