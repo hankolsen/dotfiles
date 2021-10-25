eaedc4b11bba6f7638a4e006b074922ddbd666a7
hankolsen <henrik@hankolsen.com>
2021-10-25 13:08:58 +0200

    Install docker via --cask. Remove bear

--- brew.sh
+++ brew.sh
@@ -13,14 +13,14 @@ brew install trash # rm on steroids
 brew install zsh
 
 # Docker
-brew install docker docker-compose
+# brew install docker docker-compose
+brew install --cask docker
 
 # Automatic window/pane layout for iTerm2
 brew install TomAnthony/brews/itermocil
 
 # Mac App Store CLI
 brew install mas
-mas install 1091189122 # Bear
 mas install 972028355  # Micro Snitch
 mas install 1176895641 # Spark email client
 mas install 1286743037 # Fenêtre (Picture-in-Picture)
