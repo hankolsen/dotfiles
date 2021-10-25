9882919f5841f79057707b2a8c820c2f78e78827
hankolsen <henrik@hankolsen.com>
2021-10-25 12:50:20 +0200

    brew cask is outdated, just use brew install

--- brew-cask.sh
+++ brew-cask.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-brew cask install alfred
-brew cask install atext
-brew cask install bankid
-brew cask install dropbox
-brew cask install google-chrome
-brew cask install hyper
-brew cask install inkdrop
-brew cask install little-snitch
-brew cask install mullvadvpn
-brew cask install openvpn-connect
-brew cask install postman
-brew cask install sizeup
-brew cask install station
-brew cask install sublime-text
-brew cask install visual-studio-code
+brew install alfred
+brew install atext
+brew install bankid
+brew install dropbox
+brew install google-chrome
+brew install hyper
+brew install inkdrop
+brew install little-snitch
+brew install mullvadvpn
+brew install openvpn-connect
+brew install postman
+brew install sizeup
+brew install station
+brew install sublime-text
+brew install visual-studio-code
