aed7c7511d4857ce544c97ec1c90ff9327351dae
hankolsen <henrik@hankolsen.com>
2021-10-25 12:48:51 +0200

    Add more casks

--- brew-cask.sh
+++ brew-cask.sh
@@ -5,8 +5,12 @@ brew cask install bankid
 brew cask install dropbox
 brew cask install google-chrome
 brew cask install hyper
+brew cask install inkdrop
 brew cask install little-snitch
 brew cask install mullvadvpn
+brew cask install openvpn-connect
 brew cask install postman
 brew cask install sizeup
+brew cask install station
 brew cask install sublime-text
+brew cask install visual-studio-code
